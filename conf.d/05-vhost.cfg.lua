702dda59c8f13df963afcc0d3a42f56e1693aba3
Rafa <rafa@local>
2023-08-23 15:37:58 -0600

    Update file share limits

--- conf.d/05-vhost.cfg.lua
+++ conf.d/05-vhost.cfg.lua
@@ -24,9 +24,9 @@ VirtualHost (domain)
 -- Set up a http file upload because proxy65 is not working in muc
 Component (domain_http_upload) "http_file_share"
 	  http_file_share_expires_after = 60 * 60 * 24 * 7 -- a week in seconds
-	  http_file_share_size_limit = 150*1024*1024 -- 150 MiB
-	  http_file_share_daily_quota = 200*1024*1024 -- 200 MiB per day per user
-	  http_file_share_global_quota = 5*1024*1024*1024 -- 5 GiB total
+	  http_file_share_size_limit = 350*1024*1024 -- 350 MiB
+	  http_file_share_daily_quota = 500*1024*1024 -- 500 MiB per day per user
+	  http_file_share_global_quota = 10*1024*1024*1024 -- 10 GiB total
 	  http_file_share_allowed_file_types = {"image/*","video/*","audio/*","text/*","application/*","*/*"}
 
 Component (domain_muc) "muc"
